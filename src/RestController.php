b0d9b9115d5ae3154ba50d179157d92af8580f1c
Flovet-stack <flovetoloveto@yahoo.com>
2021-05-28 17:40:08 +0100

    updated the restcontroller

--- src/RestController.php
+++ src/RestController.php
@@ -17,6 +17,23 @@ defined('BASEPATH') or exit('No direct script access allowed');
  */
 class RestController extends \CI_Controller
 {
+
+    /**
+     * Common HTTP status codes and their respective description.
+     *
+     * @link http://www.restapitutorial.com/httpstatuscodes.html
+     */
+    const HTTP_OK = 200;
+    const HTTP_CREATED = 201;
+    const HTTP_NOT_MODIFIED = 304;
+    const HTTP_BAD_REQUEST = 400;
+    const HTTP_UNAUTHORIZED = 401;
+    const HTTP_FORBIDDEN = 403;
+    const HTTP_NOT_FOUND = 404;
+    const HTTP_METHOD_NOT_ALLOWED = 405;
+    const HTTP_NOT_ACCEPTABLE = 406;
+    const HTTP_INTERNAL_ERROR = 500;
+
     /**
      * This defines the rest format
      * Must be overridden it in a controller so that it is set.
@@ -210,22 +227,6 @@ class RestController extends \CI_Controller
     protected $_enable_xss = false;
 
     private $is_valid_request = true;
-
-    /**
-     * Common HTTP status codes and their respective description.
-     *
-     * @link http://www.restapitutorial.com/httpstatuscodes.html
-     */
-    const HTTP_OK = 200;
-    const HTTP_CREATED = 201;
-    const HTTP_NOT_MODIFIED = 304;
-    const HTTP_BAD_REQUEST = 400;
-    const HTTP_UNAUTHORIZED = 401;
-    const HTTP_FORBIDDEN = 403;
-    const HTTP_NOT_FOUND = 404;
-    const HTTP_METHOD_NOT_ALLOWED = 405;
-    const HTTP_NOT_ACCEPTABLE = 406;
-    const HTTP_INTERNAL_ERROR = 500;
 
     /**
      * @var Format
